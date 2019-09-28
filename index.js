ffc02e8edf9ec9ed1c29ac2ddd46a5cf5e9b995a
Filipe Linhares <lip.linhares@gmail.com>
2019-09-28 17:52:46 -0300

    Update dependencies and start using rastrojs as service to get info

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-const micro = require('micro')
-const TrackingCorreios = require('tracking-correios')
-const url = require('url')
+const micro = require('micro');
+const { rastro } = require('rastrojs');
+const url = require('url');
 
+const capitalize = s => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
 
-module.exports = async function (req, res) {
-  const { pathname } = url.parse(req.url)
-  const code = pathname.replace('/', '').toUpperCase()
+module.exports = async function(req, res) {
+  const { pathname } = url.parse(req.url);
+  const code = pathname.replace('/', '').toUpperCase();
 
-  return await TrackingCorreios.track(code)
-}
+  const [result] = await rastro.track(code);
+
+  result.tracks = result.tracks.map(
+    ({ locale, status, trackedAt, observation, ...rest }) => {
+      const [city, state] = locale.split('/');
+      const formattedCity = capitalize(city).trim();
+      const formattedState = state.toUpperCase().trim();
+      const formattedStatus = capitalize(status);
+      const formattedTracked = trackedAt.toLocaleDateString('en-US');
+
+      const formatedObservation = observation
+        ? observation.replace(
+            new RegExp(locale, `g`),
+            `${formattedCity}/${formattedState}`,
+          )
+        : '';
+
+      const [from, to] = formatedObservation
+        .replace(`para`, `@para`)
+        .split('@');
+
+      const formattedFrom = capitalize(from);
+      const formattedTo = capitalize(to);
+
+      return {
+        locale: {
+          city: formattedCity,
+          state: formattedState,
+        },
+        observation: {
+          from: formattedFrom,
+          to: formattedTo,
+        },
+        status: formattedStatus,
+        trackedAt: formattedTracked,
+        ...rest,
+      };
+    },
+  );
+
+  return {
+    ...result,
+  };
+};
